d508b5a8585c2a3b7ef776f7b65557b72de426b8
Batuhan Ankara <sertgumecbatuhan@gmail.com>
2025-04-30 21:19:44 +0300

    models added for start

---
+++ models/header.model.js
@@ -0,0 +1,4 @@
+// headerdaki yazıların title'si en ve tr olacak
+// headerdaki yazıların destination bilgisi olacak
+//title: "anasayfa" destination: "/" veya 
+//title: "tr: İletisim" destination: "tr: /iletisim"
