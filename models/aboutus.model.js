30098aab56e77a69bb5d0f4cacd9f5a919d5eb45
kantoglu <mertkantoglu1@gmail.com>
2025-05-01 13:57:52 +0300

    aboutUs and process model added.

--- models/aboutus.model.js
+++ models/aboutus.model.js
@@ -1,3 +1,37 @@
-//image
-//title
-//content
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const multilingualFieldSchema = new Schema(
+  {
+    en: { type: String, required: true }, // İngilizce
+    tr: { type: String, required: false }, // Türkçe
+  },
+  { _id: false }
+);
+
+const paragraphSchema = new Schema(
+  {
+    title: multilingualFieldSchema,
+    subtitle: multilingualFieldSchema,
+    paragraph: multilingualFieldSchema,
+    image: { type: String, trim: true },
+  },
+  { _id: false }
+);
+
+const aboutUs = new Schema(
+  {
+    title: multilingualFieldSchema,
+    content: [paragraphSchema],
+    image: { type: Schema.Types.String, required: true },
+  },
+  {
+    minimize: true,
+    timestamps: true,
+    autoIndex: true,
+  }
+);
+
+const About = mongoose.model("About", aboutUs, "abouts");
+module.exports = About;
--- models/process.model.js
+++ models/process.model.js
@@ -1,2 +1,34 @@
 //title
 //items => title description
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const multilingualFieldSchema = new Schema(
+  {
+    en: { type: String, required: true }, // İngilizce
+    tr: { type: String, required: false }, // Türkçe
+  },
+  { _id: false }
+);
+
+const paragraphSchema = new Schema(
+    {
+      title: multilingualFieldSchema,
+      subtitle: multilingualFieldSchema,
+      paragraph: multilingualFieldSchema,
+    },
+    { _id: false })
+
+    const process = new Schema ({
+        title: multilingualFieldSchema,
+        content: [paragraphSchema],
+    },{
+        minimize: true,
+        timestamps: true,
+        autoIndex: true,
+    })
+
+    const Process = mongoose.model("Process", process, "process");
+    module.exports = Process;
